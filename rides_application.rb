c8ba284ebe42f3c046e9e264b48a73d5ab12f4a2
Stacey Fernandes <stacey.fernandes@rea-group.com>
2017-08-14 16:47:54 +1000

    WIP

--- rides_application.rb
+++ rides_application.rb
@@ -4,8 +4,12 @@ require 'json'
 class RidesApplication
   include ApplicationHelpers
 
+  def initialize(database)
+    @database = database
+  end
+
   def call(env)
-    request = Rack::Request.new(env)
+    request  = Rack::Request.new(env)
     response = Rack::Response.new
     response.headers["Content-Type"] = "application/json"
 
@@ -16,45 +20,36 @@ class RidesApplication
       get_all_rides(request, response)
     when %r{/\d+}
       get_a_ride(request, response)
-    else 
-      missing(request, response)
+    else
+      missing(response)
     end
+
     response.finish
   end
 
   def post_a_ride(request, response)
     ride = JSON.parse(request.body.read)
     if ride["user_id"].nil?
-      response.status = 400
-      response.write("user_id field is required")
+      error(response, "user_id field is required")
     else
-      Database.add_ride(ride)
-      response.write(JSON.generate({ message: "Ride received"})) 
+      @database.add_ride(ride)
+      respond_with_object(response, { message: "Ride received"})
     end
   rescue JSON::ParserError
     error(response, "Invalid JSON")
   end
 
   def get_all_rides(request, response)
-    respond_with_object(response, Database.rides)
+    respond_with_object(response, @database.rides(request.env["rides_app.user_id"]))
   end
 
   def get_a_ride(request, response)
     id = request.path_info.split("/").last.to_i
-    respond_with_object(response, Database.rides[id])
-  end
-
-  def missing(request, response)
-    response.status = 404
-    response.write("Nothing here")
-  end
-
-  def error(response, message, status = 400)
-    response.status = status
-    response.write("ERROR: #{message}")
-  end
-
-  def respond_with_object(response, object)
-    response.write(JSON.generate(object))
+    ride = @database.rides(request.env["rides_app.user_id"])[id]
+    if ride.nil?
+      error(response, "No ride with id #{id}", 404)
+    else
+      respond_with_object(response, ride)
+    end
   end
 end
